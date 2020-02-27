f9493ffc3d8ce78e533039a5bbe66ad2ed7e86d8
Jzzin <jzzzin@naver.com>
2020-02-28 08:56:17 +0900

    ch10_spring_validator complete

---
+++ ch10/spring-validator/src/main/java/ch10/config/AppConfig.java
@@ -0,0 +1,9 @@
+package ch10.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ch10")
+public class AppConfig {
+}
