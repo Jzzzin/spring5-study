29fa4e221454e3ad9e5a349163faaa2562ac66a0
Jzzin <jzzzin@naver.com>
2020-02-28 13:21:28 +0900

    ch11_async_task complete

---
+++ ch11/async-task/src/main/java/ch11/AsyncService.java
@@ -0,0 +1,9 @@
+package ch11;
+
+import java.util.concurrent.Future;
+
+public interface AsyncService {
+    void asyncTask();
+
+    Future<String> asyncWithReturn(String name);
+}
---
+++ ch11/async-task/src/main/java/ch11/config/AppConfig.java
@@ -0,0 +1,11 @@
+package ch11.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+@ComponentScan(basePackages = {"ch11"})
+public class AppConfig {
+}
