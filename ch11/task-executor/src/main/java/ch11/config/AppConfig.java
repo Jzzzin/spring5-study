d63b6b1804d023792577266518aef3c87ee26e70
Jzzin <jzzzin@naver.com>
2020-02-28 13:33:51 +0900

    ch11_task_executor complete

---
+++ ch11/task-executor/src/main/java/ch11/config/AppConfig.java
@@ -0,0 +1,19 @@
+package ch11.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+@ComponentScan(basePackages = {"ch11"})
+public class AppConfig {
+
+    @Bean
+    TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+}
