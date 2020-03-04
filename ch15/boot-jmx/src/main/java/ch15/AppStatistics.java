ccfb18c0e2593538bb8890ca48227c1661eb8a2a
Jzzin <jzzzin@naver.com>
2020-03-04 10:03:54 +0900

    ch15_boot_jmx complete

---
+++ ch15/boot-jmx/src/main/java/ch15/AppStatistics.java
@@ -0,0 +1,9 @@
+package ch15;
+
+public interface AppStatistics {
+
+    int getTotalSingerCount();
+
+    String findJohn();
+}
+
---
+++ ch15/boot-jmx/src/main/java/ch15/repos/SingerRepository.java
@@ -0,0 +1,13 @@
+package ch15.repos;
+
+import ch15.entities.Singer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface SingerRepository extends CrudRepository<Singer, Long> {
+
+    List<Singer> findByFirstName(String firstName);
+
+    List<Singer> findByFirstNameAndLastName(String firstName, String lastName);
+}
