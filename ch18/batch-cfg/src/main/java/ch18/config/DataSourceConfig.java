ac0efc9a1c4e1e3c787b9d186e4faa2578c049d8
Jzzin <jzzzin@naver.com>
2020-03-10 18:54:56 +0900

    ch18_batch_cfg complete

---
+++ ch18/batch-cfg/src/main/java/ch18/config/DataSourceConfig.java
@@ -0,0 +1,30 @@
+package ch18.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    private static Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
+
+    // <=> <jdbc:embedded-database id="dataSource" type="H2">
+    @Bean
+    public DataSource dataSource() {
+        try {
+            EmbeddedDatabaseBuilder dbBuilder = new EmbeddedDatabaseBuilder();
+            return dbBuilder.setType(EmbeddedDatabaseType.H2)
+                    .addScripts("classpath:/org/springframework/batch/core/schema-h2.sql",
+                            "classpath:support/singer.sql").build();
+        } catch (Exception e) {
+            logger.error("임베디드 DataSource 빈을 생성할 수 없습니다!", e);
+            return null;
+        }
+    }
+}
