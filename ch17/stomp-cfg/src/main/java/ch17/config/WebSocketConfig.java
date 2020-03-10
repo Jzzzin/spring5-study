f9e39f4d702f6013e0d67d93d32b3efd72b1ce9d
Jzzin <jzzzin@naver.com>
2020-03-10 13:53:34 +0900

    ch17_stomp_cfg complete

---
+++ ch17/stomp-cfg/src/main/java/ch17/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package ch17.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // <=> <websocket:stomp-endpoint .../>
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    // <=> <websocket:message-broker .../>
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic");
+    }
+}
