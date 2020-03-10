503415b7489998420ca82e9dfcef945e198f6a57
Jzzin <jzzzin@naver.com>
2020-03-10 10:55:46 +0900

    ch17_sockjs complete

---
+++ ch17/sockjs-cfg/src/main/java/ch17/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package ch17.config;
+
+import ch17.EchoHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    // <=> <websocket:handlers> <websocket:mapping .../> <websocket:sockjs/> <.../>
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(echoHandler(), "/echoHandler").withSockJS();
+    }
+
+    @Bean
+    public EchoHandler echoHandler() {
+        return new EchoHandler();
+    }
+}
